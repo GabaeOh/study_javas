7900073c364118a7a6f93544bf9e214a188f6a7a
GabaeOh <rkqo4577@gmail.com>
2023-05-23 13:14:43 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Gabae OH"; //변수를 사용하여 출력하기 String은 문자라고 말해주는 것
+        System.out.println(name);
+        // return 0;
+    }
+}
---
+++ src/Prints.java
@@ -0,0 +1,7 @@
+public class Prints {
+    public static void main(String[] args) {
+        String word = "빅데이터 java 교육 과정";
+        System.out.print(word);
+        // return 0
+    }
+}
