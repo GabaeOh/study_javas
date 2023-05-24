e639454cf3296a3b711d1426b1b1e713d6ede529
GabaeOh <rkqo4577@gmail.com>
2023-05-24 13:13:50 +0900

    과제

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+
 public class Main {
     public static void main(String[] args) {
-        String name = "Gabae OH"; //변수를 사용하여 출력하기 String은 문자라고 말해주는 것
-        System.out.println(name);
-        name = "OH";
-        System.out.println();
+        // refer : https://www.acmicpc.net/problem/2557
+        // 문제 : Hello World!를 출력하시오.
+        System.out.println( "Hello World!");
         // return 0;
+    
+
+    
+        int A = 1;
+        int B = 2;
+        System.out.println( A + B );
     }
 }
+
---
+++ src/Scanners.java
@@ -0,0 +1,20 @@
+// Scanner 외부에서 Terminal에서 값을 받을 수 있게 하는 기능
+import java.util.Scanner; // class 밖에서 선언
+public class Scanners {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in); //이 라인을 넣어야 함 
+        // Create a Scanner object
+        // System.out.println("Enter username");
+
+        // String userName = myObj.nextLine(); // Read user input
+        // System.out.println("Username is: " + userName); // Output user input
+        
+        int first = 0;
+        int second = 0;
+        first = myObj.nextInt(); //숫자를 입력할거면 nextInt를 넣어야함
+        second = myObj.nextInt();
+        System.out.println(first+","+second); // 글자를 통짜로 만들어주는 기능 +","+
+        // return 0;
+    }
+
+}
