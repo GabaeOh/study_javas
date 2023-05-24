be4d334b22c5f3e2ba68443dff7ec78c8f4a52b8
GabaeOh <rkqo4577@gmail.com>
2023-05-24 15:20:19 +0900

    for문

---
+++ src/LoopWithMark.java
@@ -0,0 +1,10 @@
+public class LoopWithMark {
+    public static void main(String[] args) {
+     
+        for (int line = 1; line < 6; line = line + 1) {
+            // code block to be executed
+            System.out.println(line);
+        }
+        System.out.println();
+    }
+}
