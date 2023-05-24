f452c2034ab4a2b8f7343892d4326fa772f0aa0f
GabaeOh <rkqo4577@gmail.com>
2023-05-24 15:29:36 +0900

    for 문 과제

--- src/LoopWithMark.java
+++ src/LoopWithMark.java
@@ -1,10 +1,10 @@
 public class LoopWithMark {
     public static void main(String[] args) {
-     
-        for (int line = 1; line < 6; line = line + 1) {
+        for (int line = 1; line < 5; line = line + 1) {
             // code block to be executed
-            System.out.println(line);
+            System.out.println("for" +""+line);
+            System.out.println();
         }
-        System.out.println();
+        System.out.println("End");
     }
 }
