9602d9bb73b82c43ef67c7df208918e276c00ed0
GabaeOh <rkqo4577@gmail.com>
2023-05-24 15:36:52 +0900

    과제

--- src/LoopWithMark.java
+++ src/LoopWithMark.java
@@ -2,8 +2,8 @@ public class LoopWithMark {
     public static void main(String[] args) {
         for (int line = 1; line < 5; line = line + 1) {
             // code block to be executed
-            System.out.println("for" +""+line);
-            System.out.println();
+            System.out.println("for" +""+line); // +**+ 더해주는 역활
+            // System.out.println(); // 라인을 바꿔줌
         }
         System.out.println("End");
     }
