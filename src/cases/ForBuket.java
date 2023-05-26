14b2c572ae02febb9de600473b845d627e5ac09e
GabaeOh <rkqo4577@gmail.com>
2023-05-26 17:15:33 +0900

    내새끼

--- src/cases/ForBuket.java
+++ src/cases/ForBuket.java
@@ -1,8 +1,26 @@
 package cases;
 
+import java.util.Scanner;
+
 public class ForBuket {
     public static void main(String[] args) {
         try {
+            // 장바구니 3가지만 담게 선언 
+            // String bucket_1 = "";
+            // String bucket_2 = "";
+            // String bucket_3 = "";
+            String[] bucket = {"","",""};  // 품목을 담을 배열 생성
+
+            Scanner myObj = new Scanner(System.in); 
+            for(int first = 0; first< bucket.length; first++){
+                System.out.print((first+1)+" 번째 담기 :"); // 원하는거 ()치기 
+                bucket[first] = myObj.nextLine();
+            }
+            
+            System.out.println("---담은 내용물---");
+            System.out.print(bucket[0]+","); // 글자가 다 붙는걸 방지 하기 위해 +"," 추가
+            System.out.print(bucket[1]+",");
+            System.out.print(bucket[2]+",");
             
         } catch (Exception e) {
             // TODO: handle exception
