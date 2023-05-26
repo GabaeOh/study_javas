b10db6bd2de65272642af39dba775657f7eb305c
GabaeOh <rkqo4577@gmail.com>
2023-05-26 15:22:55 +0900

    과제

--- src/PollsWhile.java
+++ src/PollsWhile.java
@@ -1,28 +1,24 @@
-
-
 import java.util.Scanner;
-
 public class PollsWhile {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in); // 변수 선언
+        Scanner my = new Scanner(System.in); // 변수 선언
         String answer = "";
+
         System.out.println("선택에 따라 안내글을 보여드립니다");
         System.out.println("(E)xit: 종료" + "(P)oll: 설문시작" + "(S)tatistic:설문 통계");
         System.out.println("선택입력:");
         boolean loops = true;
+
         while (loops) {
-            answer = myObj.nextLine();
-            if (answer.equals("p") || answer.equals("Poll")) {
+            answer = my.nextLine(); // 입력 받는 것 
+            if (answer.equals("p") || answer.equals("Poll")) { // 'OR'은 ||을 사용
                 System.out.println("------설문시작-----");
-                // break;
             } else if (answer.equals("s") || answer.equals("Statistic") ) {
                 System.out.println("------설문통계------");
-                // break;
             } else if (answer.equals("Exit ") ||  answer.equals("E") ) {
                 System.out.println("------설문종료-----"); 
-                break;
+                break;  // 설문종료이기에 break
             }
-            // break;
         }
     }
 }
---
+++ src/cases/ForBuket.java
@@ -0,0 +1,15 @@
+package cases;
+
+public class ForBuket {
+    public static void main(String[] args) {
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        } finally{
+
+        }
+        System.out.println();
+        // return 0; 
+    }
+}
