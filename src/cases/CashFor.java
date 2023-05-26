0f0ced3066e19e2fcf72319faefd92545f301e46
GabaeOh <rkqo4577@gmail.com>
2023-05-26 17:53:22 +0900

    과제

--- src/cases/CashFor.java
+++ src/cases/CashFor.java
@@ -16,26 +16,21 @@ public class CashFor {
 
                 // 100원 초과일 경우
                 if (bucket[first] > 100) {
-                    bucket[first]=0;
+                    bucket[first] = 0;
                     System.out.println("100초과 0원 적용");
-
                 }
 
                 // 0원으로 입력 받으면 break로 빠져나와 총합 합산
                 else if (bucket[first] == 0) {
                     break;
                 }
-
             }
 
             // 총합 구하기
             System.out.println("----담은 총합----");
-            
-
             int sum = 0;
             for (int s = 0; s < bucket.length; s++) {
                 sum += bucket[s]; // 합산한다는 표현 +=
-
             }
             System.out.println("합계" + sum);
 
