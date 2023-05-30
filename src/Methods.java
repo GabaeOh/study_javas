8a916f4f1f7a0ab72a5621e10941a196bddc16f2
GabaeOh <rkqo4577@gmail.com>
2023-05-30 17:18:22 +0900

    과제

--- src/Methods.java
+++ src/Methods.java
@@ -39,8 +39,12 @@ public class Methods {
             first = Methods.add(first, second);
             second = Methods.add(first, second);
             first = Methods.add(first, second);
+            first = Methods.add(first, second);
+            first = Methods.minus(first, second);
+            
+            int result = first; 
 
-            System.out.println(first-1);
+            System.out.println(result-1);
 
         } catch (Exception e) {
             // TODO: handle exception
