730d7b028c7f8e6e6be24f6626ab94dc1e0f996a
GabaeOh <rkqo4577@gmail.com>
2023-05-31 11:09:23 +0900

    new github second

---
+++ src/cases/GitTemplates.java
@@ -0,0 +1,14 @@
+package cases;
+
+public class GitTemplates {
+    public static void main(String[] args) {
+        try {
+            int first = 0;
+            int second = 1;
+            second = first +1;
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        //return 0;
+    }
+}
