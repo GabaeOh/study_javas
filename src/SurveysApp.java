05bde4628d3cc5c6fdc8bb8cdf1cfaf57afe3981
GabaeOh <rkqo4577@gmail.com>
2023-06-15 16:57:04 +0900

    수정

--- src/SurveysApp.java
+++ src/SurveysApp.java
@@ -8,7 +8,7 @@ public class SurveysApp {
         try {
             // - MySQL workbench 실행 : JDBC
             // - User/password와 접속 IP:port 접속
-            String url = "jdbc:mysql://127.0.0.1:3306/db_cars"; // 마지막은 db이름으로 "db_cars"
+            String url = "jdbc:mysql://127.0.0.1:3306/db_survey"; // 마지막은 db이름으로 "db_cars"
             // localhost도 가능하나 IP주소를 넣는게 보다 좋음
 
             // jdbc : java DB connetion , 'https://www.naver.com/'의 구조와 유사 / 원격으로 db_cars에 연결하는 구조를 나열
@@ -22,11 +22,18 @@ public class SurveysApp {
 
             // - query Edit
             Statement statement = connection.createStatement();
-
-            String query = " SELECT * FROM factorys"; 
-            ResultSet resultSet = statement.executeQuery(query); 
+           
+            System.out.println("---- 통계 ----");
+            // -- 총 설문자 4명 
+            String queryA = " FROM (\n" + //
+                    "\t\tSELECT RESPONDENTS_ID, count(*) CNT\n" + //
+                    "\t\tFROM statistics\n" + //
+                    "\t\tGROUP BY RESPONDENTS_ID\n" + //
+                    "        ) AS T_STATIC\n" + //
+                    ";"; 
+            ResultSet resultSet = statement.executeQuery(queryA); 
             while(resultSet.next()){ 
-                System.out.println(resultSet.getString("COMPANY_ID")+""+resultSet.getString("COMPANY") );
+                System.out.println(" -- 총 설문자 : " + resultSet.getString("CNT") );
             }
         } catch (Exception e) {
             // TODO: handle exception
