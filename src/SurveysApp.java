e26f7541dda16fde9f395514ac422c0a60ad94e0
GabaeOh <rkqo4577@gmail.com>
2023-06-15 17:17:48 +0900

    push

--- src/SurveysApp.java
+++ src/SurveysApp.java
@@ -3,12 +3,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import surveys.Statistics;
+
 public class SurveysApp {
     public static void main(String[] args) {
         try {
             // - MySQL workbench 실행 : JDBC
             // - User/password와 접속 IP:port 접속
-            String url = "jdbc:mysql://127.0.0.1:3306/db_survey"; // 마지막은 db이름으로 "db_cars"
+            String url = "jdbc:mysql://127.0.0.1:3306/db_survey"; 
             // localhost도 가능하나 IP주소를 넣는게 보다 좋음
 
             // jdbc : java DB connetion , 'https://www.naver.com/'의 구조와 유사 / 원격으로 db_cars에 연결하는 구조를 나열
@@ -23,18 +25,10 @@ public class SurveysApp {
             // - query Edit
             Statement statement = connection.createStatement();
            
-            System.out.println("---- 통계 ----");
-            // -- 총 설문자 4명 
-            String queryB = " FROM (\n" + //
-                    "\t\tSELECT RESPONDENTS_ID, count(*) CNT\n" + //
-                    "\t\tFROM statistics\n" + //
-                    "\t\tGROUP BY RESPONDENTS_ID\n" + //
-                    "        ) AS T_STATIC\n" + //
-                    ";"; 
-            ResultSet resultSet = statement.executeQuery(queryB); 
-            while(resultSet.next()){ 
-                System.out.println(" -- 총 설문자 : " + resultSet.getString("CNT") );
-            }
+            //통계 - 총 설문자 표시  
+            Statistics statistics = new Statistics(); //메서드화 시킬려면 무조건 인스턴스
+            statistics.getRespondents(statement);
+
         } catch (Exception e) {
             // TODO: handle exception
             System.out.println(e.getMessage());
